582f9b007d3b2e05e1c60544e552544efb381397
Diogo <diogopequeno5@gmail.com>
2022-09-13 11:26:31 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://laravelweb2022.herokuapp.com/api",
+    baseURL: "https://api-planta3.herokuapp.com/api",
     headers: {
         Accept: 'application/json',
     }
